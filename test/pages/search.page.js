8c2f3339bce5649224a30baea1f598f5abcf7188
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-08-14 22:09:54 -0400

    03

--- test/pages/search.page.js
+++ test/pages/search.page.js
@@ -4,8 +4,14 @@ class SearchPage extends BasePage {
 
   get searchInput() {return $('#searchval');}
   get searchBtn() {return $('button.btn.btn-info.banner-search-btn');}
-  get searchResultField() {return $$('.description.ossRelatedLinks')[0];} // should be elements from 0 to 7
-
+  get searchResultField() {return $$('.description.ossRelatedLinks');} // should be elements from 0 to 7
+  get addToCart() {return $$('.add-to-cart');}
+  get cartQuantity() {return $('#cartItemCountSpan').getText();}
+  get cartBtn() {return $('span.btn.btn-small.btn-primary');}
+  get emptyCart() {return $('a.emptyCartButton.btn.btn-mini.btn-ui.pull-right');}
+  get emptyCartPopUp() {return $('button.btn.btn-primary');}
+  get cartIsEmpty() {return $('p.header-1');}
+  get viewCart() {return $('h1.page-header.pull-left');}
 }
 
 export default new SearchPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -12,10 +12,28 @@ describe('Search page', () => {
     expect(SearchPage.searchInput.getValue()).toEqual(totalSearch.searchText)
   });
 
-  it('should check the title of the first table',  function () {
+  it('should check the title of all tables',  function () {
     SearchPage.searchBtn.click();
     browser.pause(1000)
     //SearchPage.searchResultField.isDisplayed();
-    expect(SearchPage.searchResultField.getAttribute('title')).toContain('Tables');
+    let titles = SearchPage.searchResultField.map(el => el.getAttribute('title'));
+    let titlesWithTable = titles.filter(el => el.toLowerCase().includes('table'));
+    expect(titles.length).toEqual(titlesWithTable.length);
+  });
+  it('should click add to cart ', function () {
+    let lastItem = SearchPage.addToCart[SearchPage.addToCart.length - 1];
+    browser.pause(5000)
+    lastItem.click();
+    browser.pause(5000)
+  expect(SearchPage.cartQuantity).toEqual('1');
+  });
+  it('should click on cart ', function () {
+    let cart = SearchPage.cartBtn.click()
+    browser.pause(3000)
+    let empty = viewCart.emptyCart.click()
+    browser.pause(3000)
+    let popUp = viewCart.emptyCartPopUp.click()
+    browser.pause(3000)
+    expect(viewCart.cartIsEmpty.getText())
   });
 });
