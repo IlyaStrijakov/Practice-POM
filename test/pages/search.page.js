0ac263673057c87ecf2a86ec1a08428ff8ff9b2d
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-08-17 21:48:05 -0400

    04

--- test/pages/search.page.js
+++ test/pages/search.page.js
@@ -7,11 +7,11 @@ class SearchPage extends BasePage {
   get searchResultField() {return $$('.description.ossRelatedLinks');} // should be elements from 0 to 7
   get addToCart() {return $$('.add-to-cart');}
   get cartQuantity() {return $('#cartItemCountSpan').getText();}
-  get cartBtn() {return $('span.btn.btn-small.btn-primary');}
+ // get cartBtn() {return $('span.btn.btn-small.btn-primary');}
   get emptyCart() {return $('a.emptyCartButton.btn.btn-mini.btn-ui.pull-right');}
-  get emptyCartPopUp() {return $('button.btn.btn-primary');}
+  get emptyCartPopUp() {return $("//button[text()='Empty Cart']");}
   get cartIsEmpty() {return $('p.header-1');}
-  get viewCart() {return $('h1.page-header.pull-left');}
+  get viewCart() {return $("//*[text()='View Cart']");}
 }
 
 export default new SearchPage();
